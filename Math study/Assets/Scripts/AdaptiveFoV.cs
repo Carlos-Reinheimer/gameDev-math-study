96317969e9bbdf4458575147cec73f68fe0c44ec
Carlos-Reinheimer <carlosgreinheimer@gmail.com>
2021-06-28 22:39:50 -0300

    Update AdaptiveFoV.cs

--- Math study/Assets/Scripts/AdaptiveFoV.cs
+++ Math study/Assets/Scripts/AdaptiveFoV.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class AdaptiveFoV : MonoBehaviour
 {
+    // ---------------- PART 1 --------------------
     public Transform[] points;
 
     private void OnDrawGizmos()
@@ -33,5 +34,45 @@ public class AdaptiveFoV : MonoBehaviour
         Gizmos.DrawLine(camPos, ptOutermost);
 
     }
+
+
+
+
+    // ---------------- PART 2 --------------------
+
+    //public FovPoint[] points;
+
+    //private void OnDrawGizmos()
+    //{
+    //    Camera cam = GetComponent<Camera>();
+    //    Vector2 camDir = cam.transform.forward;
+    //    Vector2 camPos = cam.transform.position;
+
+    //    float outerMostAngleRad = float.MinValue;
+    //    Vector2 ptOutermost = default;
+    //    foreach (FovPoint fovPt in points)
+    //    {
+    //        Vector2 pt = (Vector2)fovPt.transform.position - camPos;
+    //        Vector2 dirToPt = pt.normalized;
+    //        float angleToPoint = Mathf.Acos(Vector2.Dot(camDir, dirToPt));
+    //        if (angleToPoint > outerMostAngleRad)
+    //        {
+    //            outerMostAngleRad = angleToPoint;
+    //            ptOutermost = fovPt.transform.position;
+    //        }
+    //    }
+
+    //    cam.fieldOfView = outerMostAngleRad * 2 * Mathf.Rad2Deg;
+
+    //    Gizmos.DrawLine(camPos, ptOutermost);
+
+    //    DrawPointii()
+
+    //}
+
+    //void DrawPointii()
+    //{
+
+    //}
 
 }
